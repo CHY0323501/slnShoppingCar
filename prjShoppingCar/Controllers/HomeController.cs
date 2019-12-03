ebcc5b9678d6b71282aa9badc11e30863de58fda
CHY0323501 <53595498+CHY0323501@users.noreply.github.com>
2019-12-03 19:14:55 +0800

    加入專案檔案。

---
+++ prjShoppingCar/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prjShoppingCar.Models;
+
+namespace prjShoppingCar.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbShoppingCarEntities db = new dbShoppingCarEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<tProduct> p= db.tProduct.ToList();
+
+            return View(p);
+        }
+        public ActionResult MyCart()
+        {
+
+            return View();
+        }
+    }
+}
